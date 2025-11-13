75dbc38767dd37261553692b296163fecd3a277f
dBitech <darcy@dbitech.ca>
2025-11-13 11:11:32 +0000

    Deploying to docs from  @ b337ac8a6dc7712cb8f06a73a639ec96b72e4ea3 🚀

--- docs/react-maplibre/assets/LayerHoverPopup.stories-D0DgzC_2.js
+++
@@ -1,5 +0,0 @@
-import{j as r}from"./index-nGUg9nSI.js";import{b8 as e}from"./style-DnMtoM4T.js";import{m as t}from"./MapContextDecorator-BzRI4COS.js";import"./iframe-O9DyGZK0.js";import"./preload-helper-D9Z9MdNV.js";import"./index-CYTFyfAa.js";const u={title:"Hooks/LayerHoverPopup",component:e,argTypes:{},decorators:t},p=()=>r.jsx(r.Fragment,{children:r.jsx(e,{getPopupContent:()=>"Popup content"})}),o=p.bind({});o.parameters={};o.args={};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`() => {
-  return <>
-            <LayerHoverPopup getPopupContent={() => 'Popup content'} />
-        </>;
-}`,...o.parameters?.docs?.source}}};const d=["ExampleConfig"];export{o as ExampleConfig,d as __namedExportsOrder,u as default};
--- docs/react-maplibre/assets/MlMultiMeasureTool.stories-BK1nUrVj.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./index-nGUg9nSI.js";import{am as o}from"./style-DnMtoM4T.js";import{m as t}from"./MapContextDecorator-BzRI4COS.js";import"./iframe-O9DyGZK0.js";import"./preload-helper-D9Z9MdNV.js";import"./index-CYTFyfAa.js";const u={title:"MapComponents/MlMultiMeasureTool",component:o,argTypes:{},decorators:t},s=()=>e.jsx(e.Fragment,{children:e.jsx(o,{multiType:!0})}),r=s.bind({});r.parameters={};r.args={};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`() => {
-  return <>
-            <MlMultiMeasureTool multiType={true} />
-        </>;
-}`,...r.parameters?.docs?.source}}};const c=["ExampleConfig"];export{r as ExampleConfig,c as __namedExportsOrder,u as default};
