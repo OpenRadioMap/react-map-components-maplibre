82efa2d432f514e35ab78b6d7fa310ec0ce0073b
dBitech <darcy@dbitech.ca>
2026-01-02 15:36:08 +0000

    Deploying to docs from  @ 812cf2ed55843f2c837d137a23a44c9a0aae8fb4 🚀

--- docs/react-maplibre/assets/MlMultiMeasureTool.stories-BuE-mc6m.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./index-CDJtynmm.js";import{am as o}from"./style-EQxhJZjY.js";import{m as t}from"./MapContextDecorator-CZ7jaJ94.js";import"./iframe-XTL4KZp0.js";import"./preload-helper-D9Z9MdNV.js";import"./index-BVv5nCR3.js";const u={title:"MapComponents/MlMultiMeasureTool",component:o,argTypes:{},decorators:t},s=()=>e.jsx(e.Fragment,{children:e.jsx(o,{multiType:!0})}),r=s.bind({});r.parameters={};r.args={};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`() => {
-  return <>
-            <MlMultiMeasureTool multiType={true} />
-        </>;
-}`,...r.parameters?.docs?.source}}};const c=["ExampleConfig"];export{r as ExampleConfig,c as __namedExportsOrder,u as default};
